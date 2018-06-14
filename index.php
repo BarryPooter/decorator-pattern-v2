a2ace0c1b57374c9c57b53930c06a99f660bd119
Remy Kooistra <remy.kooistra@keukenmagazijn.nl>
2018-06-14 12:22:51 +0200

    Added an extra chocolate donut in the index

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 include_once 'vendor/autoload.php';
 
-$donut = new \App\Classes\SugarDonut(new \App\Classes\ChocolateDonut(new \App\Classes\ConcreteDonut()));
-$cost = number_format($donut->getCost(), 2);
+$sugarChocolateDonut = new \App\Classes\SugarDonut(new \App\Classes\ChocolateDonut(new \App\Classes\ConcreteDonut()));
+$cost = number_format($sugarChocolateDonut->getCost(), 2);
 
-echo "{$donut->getDescription()} : €{$cost}";
+echo "{$sugarChocolateDonut->getDescription()} : €{$cost}";
+echo '<br/>';
+
+$chocolateDonut = new \App\Classes\ChocolateDonut(new \App\Classes\ConcreteDonut());
+$cost = number_format($chocolateDonut->getCost(), 2);
+echo "{$chocolateDonut->getDescription()} : €{$cost}";
